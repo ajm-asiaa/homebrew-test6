95653a15249ce6a4b58debdb8e95249bd9d4c300
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2021-03-24 13:50:07 +0800

    frontend

--- Formula/carta-frontend.rb
+++ Formula/carta-frontend.rb
@@ -1,22 +1,16 @@
 class CartaFrontend < Formula
   desc "Frontend interface for CARTA"
   homepage "https://github.com/CARTAvis/carta-frontend"
-  url "http://alma.asiaa.sinica.edu.tw/_downloads/built-carta-frontend.tar.gz"
+  url "http://alma.asiaa.sinica.edu.tw/_downloads/frontend.tar.gz"
   version "2.0.0"
-  sha256 "eede630a9f9a45ea4294421782f253c7b478fd8b0d32e2f5b51fec248ae8d873"
+  sha256 "2450a8d098f4a088f2e8615721c2ee71f2fe9b194e1db47711f72bb4c4c74737"
   license "GPL-3.0-only"
-
-  bottle do
-    root_url "https://github.com/ajm-asiaa/homebrew-test6/releases/download/carta-frontend-2.0.0"
-    sha256 cellar: :any_skip_relocation, catalina:     "1fea0aa75217b6ba2889c88ea60b34f7e8549e62c5dd61dedf03585876da3f01"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "893d37d0ff4bc207c5731b1fe8f543ddac57d7af5e4e84bc0d7fb65a3146d92c"
-  end
 
   depends_on "go" => :build
 
   def install
-    mkdir_p "#{share}/carta/frontend"
-    cp_r buildpath/".", share/"carta/frontend"
+    mkdir_p "#{share}/carta"
+    cp_r buildpath/".", share/"carta/"
   end
 
   test do
