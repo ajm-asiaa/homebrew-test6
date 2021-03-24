7408f8d65df1f7ebea6eba4be0a303733b08fb18
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2021-03-24 19:06:18 +0800

    trying combined frontend and backend

---
+++ Formula/carta-beta.rb
@@ -0,0 +1,52 @@
+
+class CartaBeta < Formula
+  desc "backend and frontend components of CARTA"
+  homepage "https://cartavis.github.io/"
+  url "http://alma.asiaa.sinica.edu.tw/_downloads/carta-backend-v2.0.0-dev.21.03.05.tgz"
+  sha256 "51eb71cf6a92720d7263398e48586fadf93901fb1f436b0d0775a22766ccd086"
+  license "GPL-3.0-only"
+
+  resource "frontend" do
+    url "https://registry.npmjs.org/carta-frontend/-/carta-frontend-2.0.0-dev.21.3.05b.tgz"
+    sha256 "6cfc3a63bb917d38c41a946986cd19bc14bc5b8ece437e2e2877842b1879d55a"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "cartavis/tap/carta-casacore"
+  depends_on "cartavis/tap/carta-frontend"
+  depends_on "cartavis/tap/zfp"
+  depends_on "curl"
+  depends_on "fmt"
+  depends_on "grpc"
+  depends_on "libomp"
+  depends_on "libuv"
+  depends_on "protobuf"
+  depends_on "pugixml"
+  depends_on "tbb"
+  depends_on "zstd"
+
+  def install
+    # Grabing the pre-built carta-frontend from the npm repository.
+    resource("frontend").stage {
+      mkdir_p "#{share}/carta/frontend"
+      cp_r buildpath/"build/.", share/"carta/frontend"
+    }
+
+    # Building the carta-backend
+    ENV["OPENSSL_ROOT_DIR"] = "$(brew --prefix openssl)"
+
+    path = HOMEBREW_PREFIX/"Cellar/carta-casacore/3.0.0/include"
+
+    mkdir "build" do
+      system "cmake", "..", "-DCMAKE_PREFIX_PATH=#{lib}",
+                            "-DCMAKE_INCLUDE_PATH=#{include}",
+                            "-DCMAKE_CXX_FLAGS=-I#{path}/casacode -I#{path}/casacore",
+                            "-DCMAKE_CXX_STANDARD_LIBRARIES=-L#{lib}", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    assert_match "2.0.0-dev.21.03.04", shell_output("#{bin}/carta_backend --version")
+  end
+end
